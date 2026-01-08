966b4f93979f3dcda44b8bd6f0825770b0eddcfa
AdaleeVorbeck <adavor0529@students.maquoketaschools.org>
2026-01-08 20:11:54 +0000

    OHHHHHH SATURDAY SUN

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+let school = {
+    'name' : "Funny School Name",
+    'average class size' : 100000000,
+    'mascot' : 'middle aged man',
+}
 
+let string = `${school.name} has an average class size of ${school['average class size']} and their mascot is a ${school['middle aged man']}`
+
+document.getElementById("result").innerHTML = string;
+
+//should create an object named school that has three properties: name, average class size, and mascot
+//should assign the innerHTML of the HTML element with the id result to the  string "${name} has an average class size of ${average class size} and their mascot is a ${mascot}"
